fd0d065b12392398b00a4780de1692fa407d3e3d
Joseph Perez <hello@josephdanielperez.com>
2020-07-01 01:39:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(veggies)
 end
 
 def long_planeteer_calls(calls_long)
-  if calls_long.each.length > 4
+  if calls_long.length > 4
     return true
   else
     return false
