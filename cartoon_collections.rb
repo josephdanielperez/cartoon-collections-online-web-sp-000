01c0353cefc4854310a85bf08b19d92371bd3d9a
Joseph Perez <hello@josephdanielperez.com>
2020-07-01 01:35:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(veggies)
-  veggies.map {|name| "#{name}!"}
-  yield "#{name.upcase}!"
+  veggies.map { |veggie| "#{veggie.capitalize}!"}
 end
 
 def long_planeteer_calls# code an argument here
